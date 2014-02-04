1a9936b8f16eec5cc9f25947d848897c9f240fe7
oscar <oscarmesa.elpoli@gmail.com>
2014-02-04 08:47:33 -0500

    cambios;

--- yii-proyecto/protected/views/site/pages/empresa.php
+++
@@ -1,9 +0,0 @@
-<?php
-$this->pageTitle=Yii::app()->name . ' - Caja de Pruebas';
-$this->breadcrumbs=array(
-	'Caja de Pruebas',
-);
-?>
-<?php $this->pageTitle=Yii::app()->name; ?>
-<h1>Servicio </h1>
-<h2><?php CHtml::link('Cotizar Lentes', array('/cotizacion/create')); ?></h2>
